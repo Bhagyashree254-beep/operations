9623299df8cfb646d3e629c15224f183c717ad8a
bhagyashree.mahagaonkar <01fe24bca254@kletech.ac.in>
2026-01-19 16:25:46 +0530

    program

--- operations.py
+++ operations.py
@@ -1,20 +1,32 @@
 import sys
 
-def operations(a, b):
-    return a + b, a - b, a * b, a / b
+def addition(a, b):
+    return a + b
+def substraction(a, b):
+    return a - b
+def multiplication(a, b):
+    return a * b
+def division(a, b):
+    return a / b
 
 if __name__ == "__main__":
     try:
         a = int(sys.argv[1])
         b = int(sys.argv[2])
     except IndexError:
-        a = int(input("Enter first num1: "))
-        b = int(input("Enter second num2: "))
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
     except ValueError:
         print("Please enter valid integers.")
         sys.exit(1)
-
-    print(operations(a, b))
-
+        
+    print("\n.....Result...\n") 
+    print("sum", addition(a,b))
+    print("sub", substraction(a,b))
+    print("multi", multiplication(a,b))
+    print("div", division(a,b))
+    
+    
+   
 
 
